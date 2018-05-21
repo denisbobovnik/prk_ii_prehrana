59ecb84041cc7536f708f119427c1df0a6451668
Denis Bobovnik <denis.bobovnik@gmail.com>
2018-05-21 13:59:08 +0200

    prehrana BEGIN

---
+++ prk_ii_prehrana/src/si/um/feri/prk/jsfbeans/PrehranaJSFBean.java
@@ -0,0 +1,16 @@
+package si.um.feri.prk.jsfbeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@ManagedBean(name="PrehranaJSFBean")
+@SessionScoped
+public class PrehranaJSFBean {
+	
+	Logger log=LoggerFactory.getLogger(ClanekJSFBean.class);
+
+	//metode
+
+}
