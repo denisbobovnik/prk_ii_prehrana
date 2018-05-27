4741b5e0e5f0e47190e67da09f8e98b9fde51239
DESKTOP-9MMTD6A\Administrator <verica.zagoranski@student.um.si>
2018-05-27 16:57:27 +0200

    Vnos-programa.xhtml, ProgramJSFBean in sprememba pri dodajanju sestavine

--- prk_ii_prehrana/src/si/um/feri/prk/jsfbeans/ProgramJSFBean.java
+++ prk_ii_prehrana/src/si/um/feri/prk/jsfbeans/ProgramJSFBean.java
@@ -2,6 +2,7 @@ package si.um.feri.prk.jsfbeans;
 
 import java.security.Principal;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import javax.ejb.SessionContext;
@@ -18,7 +19,9 @@ import org.slf4j.LoggerFactory;
 import si.um.feri.prk.dao.ClanekDAO;
 import si.um.feri.prk.dao.ProgramDAO;
 import si.um.feri.prk.objekti.Clanek;
+import si.um.feri.prk.objekti.Enota;
 import si.um.feri.prk.objekti.Program;
+import si.um.feri.prk.objekti.Recept;
 
 @ManagedBean(name="ProgramJSFBean")
 @SessionScoped
@@ -27,10 +30,87 @@ public class ProgramJSFBean {
 	
 	Logger log=LoggerFactory.getLogger(ProgramJSFBean.class);
 	private ProgramDAO pD = ProgramDAO.getInstance();
+	private Program p = new Program();
 	private Program izbranProgram = new Program();
+	private UploadedFile thumbnail;
+	private Enota enota = new Enota();
+	private Recept izbranRecept = new Recept();
+	//private ArrayList<Enota> enote = new ArrayList();
 	
+	public void dodajProgram() {
+		try {
+			String str = thumbnail.getFileName();
+			if(str.contains(".")) {
+				String ext = str.substring(str.lastIndexOf('.'), str.length());
+				if(ext.equalsIgnoreCase(".jpg")||(ext.equalsIgnoreCase(".png"))||(ext.equalsIgnoreCase(".jpeg"))||(ext.equalsIgnoreCase(".gif"))) {
+					p.setSlika(thumbnail.getContents());
+					pD.shrani(p);
+					p = new Program();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			FacesMessage errorMsg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Napaka nalaganja!", e.getMessage());
+			FacesContext.getCurrentInstance().addMessage(null, errorMsg);
+		}
+	}
 
+	public void dodajEnoto() {
+		p.getEnote().add(enota);
+		enota = new Enota();
+	}
+	public void dodajRecept() {
+		enota.getRecepti().add(izbranRecept);
+		izbranRecept = new Recept();
+	}
 	
+	public Recept getIzbranRecept() {
+		return izbranRecept;
+	}
+
+	public void setIzbranRecept(Recept izbranRecept) {
+		this.izbranRecept = izbranRecept;
+	}
+	public Enota getEnota() {
+		return enota;
+	}
+
+	public void setEnota(Enota enota) {
+		this.enota = enota;
+	}
+
+	public UploadedFile getThumbnail() {
+		return thumbnail;
+	}
+
+	public void setThumbnail(UploadedFile thumbnail) {
+		this.thumbnail = thumbnail;
+	}
+
+	public ProgramDAO getpD() {
+		return pD;
+	}
+
+	public void setpD(ProgramDAO pD) {
+		this.pD = pD;
+	}
+
+	public Program getP() {
+		return p;
+	}
+
+	public void setP(Program p) {
+		this.p = p;
+	}
+
+	public Program getIzbranProgram() {
+		return izbranProgram;
+	}
+
+	public void setIzbranProgram(Program izbranProgram) {
+		this.izbranProgram = izbranProgram;
+	}
+
 	public void izberiProgram(int id_program) {
 		log.info("ProgramJSFBean: izberiProgram");
 		try {
