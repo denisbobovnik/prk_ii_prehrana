6053bbb544ac34f82d57e2fcb1c5870410527fac
DESKTOP-9MMTD6A\Administrator <verica.zagoranski@student.um.si>
2018-06-01 23:56:50 +0200

    Vnos namiga(ni dokoncano)

---
+++ prk_ii_prehrana/src/si/um/feri/prk/jsfbeans/NamigJSFBean.java
@@ -0,0 +1,13 @@
+package si.um.feri.prk.jsfbeans;
+
+public class NamigJSFBean {
+	private String namig;
+
+	public String getNamig() {
+		return namig;
+	}
+
+	public void setNamig(String namig) {
+		this.namig = namig;
+	}
+}
