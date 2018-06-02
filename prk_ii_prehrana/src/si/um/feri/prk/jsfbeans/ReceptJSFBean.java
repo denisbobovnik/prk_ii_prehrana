482630d14cc5c510750ece234b94ec59b6e74a40
Denis Bobovnik <denis.bobovnik@gmail.com>
2018-06-02 14:24:41 +0200

    Omogočeno sortiranje vseh receptov po kategoriji.

--- prk_ii_prehrana/src/si/um/feri/prk/jsfbeans/ReceptJSFBean.java
+++ prk_ii_prehrana/src/si/um/feri/prk/jsfbeans/ReceptJSFBean.java
@@ -43,6 +43,44 @@ public class ReceptJSFBean {
 	private ArrayList<Sestavine> sestavineTrenutnegaRecepta = new ArrayList<Sestavine>();
 	private Recept urejenRecept = new Recept();
 	private StringBuilder alergeniZVejicami;
+	
+	private String izbranaKategorijaReceptov;
+	
+	public ArrayList<String> vrniVseKategorijeReceptov() throws Exception {
+		ArrayList<Recept> vsiRecepti = rD.vrniVse();
+		ArrayList<String> ret = new ArrayList<String>();
+		for(Recept r : vsiRecepti)
+			if(!ret.contains(r.getKategorija()))
+				ret.add(r.getKategorija());
+		return ret;
+	}
+	
+	public ArrayList<Recept> vrniRecepteZaPrikaz() throws Exception {
+		ArrayList<Recept> ret = new ArrayList<Recept>();
+		if(izbranaKategorijaReceptov.equals("Vsi"))
+			return rD.vrniVse();
+		else
+			for(Recept rec : rD.vrniVse())
+				if(rec.getKategorija().equals(izbranaKategorijaReceptov))
+					ret.add(rec);
+		return ret;
+	}
+	
+	public void ponastaviKategorijo() {
+		izbranaKategorijaReceptov = null;
+	}
+	
+	public void nastaviKategorijo(String kategorija) {
+		izbranaKategorijaReceptov = kategorija;
+	}
+	
+	public String getIzbranaKategorijaReceptov() {
+		return izbranaKategorijaReceptov;
+	}
+
+	public void setIzbranaKategorijaReceptov(String izbranaKategorijaReceptov) {
+		this.izbranaKategorijaReceptov = izbranaKategorijaReceptov;
+	}
 
 	public void dodajRecept() {
 		try {
