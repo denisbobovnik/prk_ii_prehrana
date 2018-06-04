dd5242ce42bf07fde50b5415682f783e59078982
DESKTOP-9MMTD6A\Administrator <verica.zagoranski@student.um.si>
2018-06-04 14:54:58 +0200

    Spremenjen NamigJSFBean

--- prk_ii_prehrana/src/si/um/feri/prk/jsfbeans/NamigJSFBean.java
+++ prk_ii_prehrana/src/si/um/feri/prk/jsfbeans/NamigJSFBean.java
@@ -11,66 +11,31 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-public class NamigJSFBean {
-	private String namig;
+import si.um.feri.prk.dao.NamigDAO;
+import si.um.feri.prk.dao.PrehranaDAO;
+import si.um.feri.prk.objekti.Namig;
 
+public class NamigJSFBean {
+	private Namig namig = new Namig();
+	private Namig izbranNamig = new Namig();
+	private NamigDAO nD = NamigDAO.getInstance();
 	
 	
-	public String getNamig() {
+	public Namig getNamig() {
 		return namig;
 	}
-
-	public void setNamig(String namig) {
+	public void setNamig(Namig namig) {
 		this.namig = namig;
 	}
-	
-	private String getUserRole(){
-		FacesContext context = FacesContext.getCurrentInstance();
-		String[] vloge = {"ADMINISTRATOR", "STROKOVNJAK", "POSAMEZNIK"};
-		String ret = "";
-		for(String s : vloge)
-			if(context.getExternalContext().isUserInRole(s))
-				ret = s;
-		return ret;
+	public Namig getIzbranNamig() {
+		return izbranNamig;
+	}
+	public void setIzbranNamig(Namig izbranNamig) {
+		this.izbranNamig = izbranNamig;
 	}
 	
-	public void poslji() {
-     		String obvestilo = namig;
-     	    String m = "praktikumekd@gmail.com";
-    		String g="praktikumEKD1997";
-    		String mail = null;
 	
-			
-			Properties props = new Properties();
-			props.put("mail.smtp.auth", "true");
-			props.put("mail.smtp.starttls.enable", "true");
-			props.put("mail.smtp.host", "smtp.gmail.com");
-			props.put("mail.smtp.port", "587");
-
-			Session session = Session.getDefaultInstance(props,
-				new javax.mail.Authenticator() {
-					protected PasswordAuthentication getPasswordAuthentication() {
-						return new PasswordAuthentication(m,g);
-					}
-				});
-
-			try {
-
-				Message message = new MimeMessage(session);
-				message.setFrom(new InternetAddress(m));
-				message.setRecipients(Message.RecipientType.TO,
-					InternetAddress.parse(mail));
-				message.setSubject("Registracija");
-				message.setText(obvestilo);
-
-				Transport.send(message);
-
-				
-
-			} catch (MessagingException e) {
-				throw new RuntimeException(e);
-			}
-		
-	}
+	
+	
 	
 }
